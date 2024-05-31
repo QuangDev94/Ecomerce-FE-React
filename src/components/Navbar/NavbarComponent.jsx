47e8ab175c3463b9c3bd3da6d192a6f867dc06da
QuangDev <ng.quang.coder.new@gmail.com>
2024-05-31 17:03:50 +0700

    Build base Navbar UI

---
+++ src/components/Navbar/NavbarComponent.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { WrapperContent, WrapperLabelText, WrapperTextPrice, WrapperTextValue } from './style';
+import { Checkbox, Rate } from 'antd';
+
+const NavbarComponent = () => {
+  const renderContent = (type,options) => {
+    switch (type) {
+      case 'text' :
+        return options.map((option) => {
+          return <WrapperTextValue>{option}</WrapperTextValue>
+        });
+      case 'checkbox':
+        return (
+          <Checkbox.Group style={{ width: '100%',display: 'flex', flexDirection: 'column',gap: '12px' }}>
+            {
+              options.map((option) => {
+                return <Checkbox value={option.value}>{option.lable}</Checkbox>
+              })
+            }
+          </Checkbox.Group>
+        )
+      case 'star': 
+        return (
+          options.map((option) => {
+            return  <>
+                      <Rate style={{fontSize: '12px'}} allowHalf defaultValue={option} />
+                      <span style={{fontSize: '11px'}}>{`from ${option} star`}</span>
+                    </>
+            })
+        )
+      case 'price':
+        return (
+          options.map((option) => {
+            return  <WrapperTextPrice>{option}</WrapperTextPrice>
+          })
+        )
+      default: 
+        return <></>
+    }
+  }
+  return (
+    <div style={{backgroundColor: '#fff'}}>
+      <WrapperLabelText>Label</WrapperLabelText>
+      <WrapperContent>
+        {renderContent('text', ['Dell','Iphone','Samsung'])}
+      </WrapperContent>
+      <WrapperContent>
+        {renderContent('checkbox', [{value: 'a',lable: 'A'},{value: 'b',lable: 'B'},{value: 'c',lable: 'C'}])}
+      </WrapperContent>
+      <WrapperContent>
+        {renderContent('star', [3,4,5])}
+      </WrapperContent>
+      <WrapperContent>
+        {renderContent('price', ['under 40.000','over 40.000'])}
+      </WrapperContent>
+    </div>
+  )
+}
+
+export default NavbarComponent
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -7,6 +7,7 @@ import slider2 from '../../assets/images/slider2.jpg';
 import slider3 from '../../assets/images/slider3.jpg';
 import slider4 from '../../assets/images/slider4.jpg';
 import CardComponent from '../../components/Card/CardComponent';
+import NavbarComponent from '../../components/Navbar/NavbarComponent';
 
 const HomePage = () => {
   const arr = ['Iphone','Ipad','SamSung']
@@ -23,12 +24,12 @@ const HomePage = () => {
           }
         </WrapperTypeProduct>
       </div>
-      <div id='container' style={{padding: '0 120px',backgroundColor: "#efefef",height: "1000px"}}>
+      <div id='container' style={{padding: '0 120px',backgroundColor: "#efefef",height: "10000px"}}>
         <SliderComponent arrImage={[slider1,slider2,slider3,slider4]} />
         <div style={{marginTop:"20px", display: "flex", alignItems: "center", gap: "20px"}}>
           <CardComponent />
-
         </div>
+      < NavbarComponent /> 
       </div>
     </>
   )
